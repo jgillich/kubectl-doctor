4dd91b3d5328c8a64dd9af25e91c92f2d7b92e89
Jakob Gillich <jakob@gillich.me>
2023-09-18 12:16:50 +0000

    feat: add severity in rules cmd

--- cmd/kubectl-doctor/rules.go
+++ cmd/kubectl-doctor/rules.go
@@ -16,9 +16,9 @@ var cmdRules = &cobra.Command{
 		w := printers.GetNewTabWriter(os.Stdout)
 		defer w.Flush()
 
-		fmt.Fprintf(w, "RULE\tDESCRIPTION\n")
+		fmt.Fprintf(w, "RULE\tSEVERITY\tDESCRIPTION\n")
 		for _, tr := range triage.List {
-			fmt.Fprintf(w, "%s\t%s\n", triage.Id(tr), tr.Description())
+			fmt.Fprintf(w, "%s\t%s\t%s\n", triage.Id(tr), tr.Severity().String(), tr.Description())
 		}
 	},
 }
